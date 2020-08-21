e478e4470b53403b6c3ff279878479b62a46efc4
okapeeokapee <okapee420@gmail.com>
2020-08-21 13:08:28 +0900

    input要素に入力された値を追加していく

--- js/main.js
+++ js/main.js
@@ -11,9 +11,17 @@
 
   document.querySelector('button').addEventListener('click', ()=>{
 
-    const item1=document.querySelectorAll('li')[1];
+    const li=document.createElement('li');
+    const text=document.querySelector('input');
+    li.textContent=text.value;
+    document.querySelector('ul').appendChild(li);
+
+    text.value='';
+    text.focus();
+
+    // const item1=document.querySelectorAll('li')[1];
     // item1.remove();
-    document;querySelector('ul').removeChild(item1);
+    // document;querySelector('ul').removeChild(item1);
     // const item0 = document.querySelectorAll('li')[0];
     // const copy = item0.cloneNode(true);
 
